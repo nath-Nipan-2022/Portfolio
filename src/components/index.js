5425ffcb459a503ca6ff35b6a85356dd3ddb5459
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-31 23:45:12 +0530

    ✨create components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import { Navbar } from "./Navbar";
+import { Hero } from "./Hero";
+import { About } from "./About";
+import { Tech } from "./Tech";
+import { Contact } from "./Contact";
+import { StarsCanvas } from "./StarsCanvas";
+
+export { Navbar, About, Contact, Hero, Tech, StarsCanvas };
