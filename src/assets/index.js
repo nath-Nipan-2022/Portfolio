a957fb83d00d24ed029d7d9479337c30cfc4e701
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-31 23:44:08 +0530

    add assets / icons

---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import ecommerce from "./e-commerce.png";
+import movieApp from "./movieApp.png";
+
+export {
+  logo,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  ecommerce,
+  movieApp,
+};
