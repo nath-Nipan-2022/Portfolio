39c697f40e666cde0a6484c81178f6a9205d3a0b
Nipan Debnath <debnathnipan719@gmail.com>
2023-11-03 23:58:16 +0530

    app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { About, Contact, Hero, Navbar, StarsCanvas, Tech } from "./components";
+
 function App() {
-  return (
-    <div className="">
-      <h1>Web Developer Portfolio</h1>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div className="relative z-0">
+          <Navbar />
+          <main>
+            <Hero />
+            <About />
+            <Tech />
+            <div className="relative z-0">
+              <Contact />
+              <StarsCanvas />
+            </div>
+          </main>
+        </div>
+      ),
+      errorElement: (
+        <div className="text-red-900 bg-red-500">404 page not found</div>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
