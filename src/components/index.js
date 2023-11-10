9909949b8e1bdfea2e89c38f1f949fd487b4bb57
Nipan Debnath <debnathnipan719@gmail.com>
2023-11-10 14:56:46 +0530

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,20 @@ import { Navbar } from "./Navbar";
 import { Hero } from "./Hero";
 import { About } from "./About";
 import { Tech } from "./Tech";
+import { Works } from "./Works";
 import { Contact } from "./Contact";
-import { StarsCanvas } from "./StarsCanvas";
+import { StarsCanvas } from "./canvas";
+import { LoadingScreen } from "./LoadingScreen";
+import { Footer } from "./Footer";
 
-export { Navbar, About, Contact, Hero, Tech, StarsCanvas };
+export {
+  Navbar,
+  About,
+  Contact,
+  Hero,
+  Tech,
+  Works,
+  StarsCanvas,
+  LoadingScreen,
+  Footer,
+};
