aab931d81ef1a79cc647ebb6907d2f41a90a8055
Nipan Debnath <debnathnipan719@gmail.com>
2023-12-16 23:24:57 +0530

    update assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -18,7 +18,7 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
 
 import ecommerce from "./e-commerce.webp";
 import movieApp from "./movieApp.webp";
